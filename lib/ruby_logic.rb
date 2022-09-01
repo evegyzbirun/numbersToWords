9b8e07cff8cd4b1620643b7c844f874c1dc31645
evegyzbirun <zbirunzbirun@gmail.com>
2022-08-31 20:55:41 -0700

    small fix.

--- lib/ruby_logic.rb
+++ lib/ruby_logic.rb
@@ -28,9 +28,15 @@
       60 => "sixty",
       70 => "seventy",
       80 => "eighty",
-      90 => "ninety",
+      90 => "ninety"
+      
     }
+    # 2 ,3 ,4 ,5 
+    # 2345 
+    # if length is 4 we split 2 345 if i[1] has length is 3 we add "thousand" after number(i[0]) and we split i[1] on 3 45 if i[1].[1]  length is 2 .number we add "hundred" after i[1].[0] number(i[1].[0]) and if i[1].[1] 45 is not from 1 to 19 we split and get 4 i[1].[1].[0] and 5 i[1].[1].[0] and we add 0 to i[1].[1].[0] move to numbers and i[1].[1].[0] moce to numbers. we concat each step to new object. 
 
+   # if (input )
+    
     # 9 100,000,000
     # for (i=length; i > 0; i--)
     # if i == 6 = "million"  |
@@ -43,10 +49,11 @@
     # if (input <= 19) 
     #   return numbers[input]
 
-    elsif (input > 19 && input < 100)
+    if (input > 19 && input < 100)
       return_string = ''
       string = input.to_s
       array = string.split('') #----> take first piece, add "0" now it's going to be "30" then send to numbers hash
+      #[2,1] = [20, 1]
       tens_place = array[0].concat("0")
       if (tens_place != "0")
         return_string.concat(numbers[tens_place.to_i])
